ab8adb8dade282d51f90bafaef42d12b8f0ff332
Tobias Franek <tobias.franek@gmail.com>
2015-08-18 22:21:41 +0200

    [BE][FE] added favorite filter so only favorites show up on landing

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,6 +3,11 @@
 class LandingController < ApplicationController
   def index
     @favorites = Favorites.where(:user_id => session[:user_id])
-    @music = @favorites[:music_id].collect {|i| Music.where(:id => i) }.flatten
+
+    favoriteMusic = []
+    @favorites.each do |favorite|
+      favoriteMusic = favorite.music_id
+    end
+    @music = Music.where(id: favoriteMusic)
   end
 end
