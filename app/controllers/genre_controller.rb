245b8ee5622a73c5d53faae7895eb5d5dc83142d
Tobias Franek <tobias.franek@gmail.com>
2015-08-22 12:26:46 +0200

    [BE] added all to genres

--- app/controllers/genre_controller.rb
+++ app/controllers/genre_controller.rb
@@ -4,21 +4,38 @@ class GenreController < ApplicationController
   end
 
   def show
-    @favorites = Favorites.where(user_id: session[:user_id])
+    if params[:genre_id] == 'all'
+      @favorites = Favorites.where(user_id: session[:user_id])
 
-    favoriteMusic = []
-    @favorites.each do |favorite|
-      favoriteMusic.push(favorite.music_id)
-    end
-    @musicFavoured = Music.where(id: favoriteMusic, genre_id: params[:genre_id])
-    @musicUnfavoured = Music.where.not(id: favoriteMusic).where(genre_id: params[:genre_id])
-    @musicFavoured.each do |favorite|
-      favorite.ifFavorite = true
-    end
-    @musicUnfavoured.each do |favorite|
-      favorite.ifFavorite = false
-    end
-    @music = (@musicFavoured + @musicUnfavoured).sort_by(&:title)
+      favoriteMusic = []
+      @favorites.each do |favorite|
+        favoriteMusic.push(favorite.music_id)
+      end
+      @musicFavoured = Music.where(id: favoriteMusic)
+      @musicUnfavoured = Music.where.not(id: favoriteMusic)
+      @musicFavoured.each do |favorite|
+        favorite.ifFavorite = true
+      end
+      @musicUnfavoured.each do |favorite|
+        favorite.ifFavorite = false
+      end
+      @music = (@musicFavoured + @musicUnfavoured).sort_by(&:title)
+    else
+      @favorites = Favorites.where(user_id: session[:user_id])
 
+      favoriteMusic = []
+      @favorites.each do |favorite|
+        favoriteMusic.push(favorite.music_id)
+      end
+      @musicFavoured = Music.where(id: favoriteMusic, genre_id: params[:genre_id])
+      @musicUnfavoured = Music.where.not(id: favoriteMusic).where(genre_id: params[:genre_id])
+      @musicFavoured.each do |favorite|
+        favorite.ifFavorite = true
+      end
+      @musicUnfavoured.each do |favorite|
+        favorite.ifFavorite = false
+      end
+      @music = (@musicFavoured + @musicUnfavoured).sort_by(&:title)
+    end
   end
 end
