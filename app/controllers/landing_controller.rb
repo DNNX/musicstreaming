5d8f6b992c0c3e45a453a67502b62f245d0dc369
Viktar Basharymau <6alliapumob@gmail.com>
2015-09-23 20:39:14 +0300

    Simplify LandingController a little
    `map` is more idiomatic than `each` & `push`/`<<`

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -4,9 +4,8 @@ class LandingController < ApplicationController
   def index
     @favorites = Favorites.where(user_id: session[:user_id])
 
-    favoriteMusic = []
-    @favorites.each do |favorite|
-      favoriteMusic.push(favorite.music_id)
+    favoriteMusic = @favorites.map do |favorite|
+      favorite.music_id
     end
     @music = Music.where(id: favoriteMusic)
   end
